cc2e48ba26b7cd3fe69bafc80939fa0430414eff
naeliofreires <naeliofreires@gmail.com>
2017-04-09 22:11:16 -0300

    finalizado

--- src/WebServer.java
+++ src/WebServer.java
@@ -16,21 +16,21 @@ public final class WebServer {
 		// Criando o Socket do Cliente
 		Socket socketCli;
 		
-//		Como estaremos atendendo a mensagens de requisição indefinitivamente 
-//		colocaremos em um loop infinito
+		//Como estaremos atendendo a mensagens de requisição indefinitivamente 
+		//colocaremos em um loop infinito
 		while (true) {
 		
 			//Apenas um mensagem para indicar que o servidor encontra-se ativo
 			System.out.println( "Servidor Ativo" );
 			
-//			Ficar escutando o pedido de Conexão
+			//Ficar escutando o pedido de Conexão
 			socketCli = socketServ.accept();
 			
-//			Passamos ao construtor um objeto 'socket' que representa nossa conexão
-//			com o cliente
+			//Passamos ao construtor um objeto 'socket' que representa nossa conexão
+			//com o cliente
 			HttpRequest requisicao = new HttpRequest(socketCli);
 			
-//			Criado um novo thread para manipular as requisicões
+			//Criado um novo thread para manipular as requisicões
 			Thread thread = new Thread (requisicao);
 			
 			//Inicia o thread.
