89ec796c661db983e38f331bec226da267585964
naeliofreires <naeliofreires@gmail.com>
2017-04-09 21:38:05 -0300

    Finalizado a main

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,32 +1,36 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+
+//Esta classe faz a conexão com o cliente e deixa o tratamento 
+//sobre responsabilidade de outra classe
 public final class WebServer {
 	
 	//método que executa o inicio do programa
 	public static void main (String arvg[]) throws Exception{
 	
-		//Porta 8181, escolhida sem motivo especial
-		int porta = 8181;
+		int porta = 8181;// Escolhemos primeiro a porta (sem motivo especial) 
 		
-		//Estabelece o socket de escuta do Servidor
+		// Criando um Socket Servidor que inicia a conexão e passamos a porta criado ...
 		ServerSocket socketServ = new ServerSocket(porta);
 		
-		//Estabelece o socket do cliente
+		// Criando o Socket do Cliente
 		Socket socketCli;
 		
-		//Processar a requisicao do servico HTTP em um laco infinito
+//		Como estaremos atendendo a mensagens de requisição indefinitivamente 
+//		colocaremos em um loop infinito
 		while (true) {
 		
 			//Apenas um mensagem para indicar que o servidor encontra-se ativo
 			System.out.println( "Servidor Ativo" );
 			
-			//Escutar requisicao de conexao TCP.
+//			Ficar escutando o pedido de Conexão
 			socketCli = socketServ.accept();
 			
-			//Constroi um objeto para processar a mensagem de requisicao HTTP
+//			Passamos ao construtor um objeto 'socket' que representa nossa conexão
+//			com o cliente
 			HttpRequest requisicao = new HttpRequest(socketCli);
 			
-			//Criado um novo thread para processar as novas requisicões
+//			Criado um novo thread para manipular as requisicões
 			Thread thread = new Thread (requisicao);
 			
 			//Inicia o thread.
