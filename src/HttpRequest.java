69f22edabe5af7dbaa91ea6a1abb0093f0112bbc
naeliofreires <naeliofreires@gmail.com>
2017-04-09 22:08:58 -0300

    add processRequest()

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,19 +1,24 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class HttpRequest implements Runnable {
 
 	//Carriage Return + Line Feed
 	final static String CRLF = "\r\n";
+	
 	//referência do socket da conexão
 	Socket socket;
-	//senha para autenticacao de pasta protegida
-	private String senhaParaAutenticacao = "redes:computadores";
-	//Construtor
+	
 	public HttpRequest (Socket socket) throws Exception{
 		this.socket = socket;
 	}
+	
 	// roda processrequest e trata as exceções
 	public void run(){
+	
 		try{
 			processRequest();
 		}
@@ -21,8 +26,35 @@ public class HttpRequest implements Runnable {
 			System.out.println (e);
 		}
 	}
-	private void processRequest() {
-		// TODO Auto-generated method stub
+	
+	private void processRequest() throws IOException {
 		
+		//Primeiro obtemos referências para os trechos de entrada e saída do socket.
+
+		//Objeto isr referência para os trechos de entrada
+		InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+		
+		//Objeto dos referência para os trechos de saida
+		DataOutputStream dos = new 	DataOutputStream(socket.getOutputStream());
+		
+		//Ajustar os filtros do trecho de entrada
+		BufferedReader br = new BufferedReader(isr);
+		
+		//Obter a linha de requisição da mensagem de requisição HTTP
+		String requestLine = br.readLine();
+		
+		//Exibir a linha de requisição no Console
+		System.out.println(); // pula uma linha
+		System.out.println(requestLine);
+		
+		String headerLine = null;
+		
+		while ((headerLine = br.readLine()).length() != 0){
+			System.out.println(headerLine);
+		}
+		
+		isr.close();
+		dos.close();
+		socket.close();
 	}
 }
